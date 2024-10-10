56b112cc01c207fdea9669cad8f1ff3159063af4
Sagnik <sagnikmis.datta@gmail.com>
2024-10-10 23:04:06 +0530

    Modified

---
+++ run.py
@@ -0,0 +1,12 @@
+import base64
+
+# Path to your Firebase Admin Key JSON
+json_file_path = "imageria-a09ae-firebase-adminsdk-whbrs-b42c754357.json"
+
+# Read the file and encode it
+with open(json_file_path, "rb") as f:
+    encoded_data = base64.b64encode(f.read())
+
+# Convert bytes to string and print the Base64 string
+base64_string = encoded_data.decode('utf-8')
+print(base64_string)
